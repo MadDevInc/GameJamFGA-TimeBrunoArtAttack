89581c2e67702a00b2c2dca90358b5cd01c5029a
RufinoVfR <200062891@aluno.unb.br>
2024-11-05 14:46:13 -0300

    fix

--- NecroGame/necro/Scenes/Player/player.gd
+++ NecroGame/necro/Scenes/Player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 10.0
+const SPEED = 50.0
 
 func _physics_process(delta):
 
@@ -13,13 +13,14 @@ func _physics_process(delta):
 
 	if direction_x:
 		velocity.x = direction_x * SPEED
+		print(velocity)
 		#if direction_x == "left":
 			#$AnimatedSprite2D.play("moving_sides")
 		#else:
 			#$AnimatedSprite2D.play("moving_sides")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		#$AnimatedSprite2D.play("moving_sides")
+		
 
 	
 	if direction_y:
@@ -27,6 +28,6 @@ func _physics_process(delta):
 		$AnimatedSprite2D.play("moving_up_down")
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-		#$AnimatedSprite2D.play("idle")
+		
 
 	move_and_slide()

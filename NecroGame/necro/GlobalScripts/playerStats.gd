1219e8f43dd5b4fa38d736175d0d2eb86fc6ff02
matix <mateusviniciuspw@gmail.com>
2024-11-06 18:13:53 -0300

    add

---
+++ NecroGame/necro/GlobalScripts/playerStats.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var xp = 0;
+var vida = 3;
+var dano = 1;
+var isSummonLiberado = false;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

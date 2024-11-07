aff3decd61754f11517dfb05c9eaf377e2d5159d
Matix <mateusviniciuspw@gmail.com>
2024-11-07 17:29:23 -0300

    portal funcionando

--- NecroGame/necro/Scenes/World/Hub/hub.gd
+++ NecroGame/necro/Scenes/World/Hub/hub.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+var cenaMundo = load("res://Scenes/World/world.tscn")
+var podeRalar: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +10,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	exit()
+
+func _on_portal_body_entered(body):
+	podeRalar = true
+
+func _on_portal_body_exited(body):
+	podeRalar = false
+	
+func exit():
+	if Input.is_action_pressed("interact"):
+		if podeRalar == false:
+			pass
+		if podeRalar == true:
+			get_tree().change_scene_to_packed(cenaMundo)
